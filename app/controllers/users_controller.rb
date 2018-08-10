54f897fa53dcebf21ea74a4193643dff13b82f84
tmquang1999 <quang99991111@gmail.com>
2018-08-10 20:17:28 +0700

    advlogin
    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       flash[:success] = 'success'
       redirect_to @user
     else
