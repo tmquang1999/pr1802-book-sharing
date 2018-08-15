4ea570fb085ac0d8844b88803d605f6e26b760fd
tmquang1999 <quang99991111@gmail.com>
2018-08-16 00:24:31 +0700

    resetpass
    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'password_reset/new'
+  get 'password_reset/edit'
   root 'static_pages#home'
   get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
@@ -9,4 +13,5 @@ Rails.application.routes.draw do
   resources :users
   resources :books,  only: [:create, :destroy]
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
