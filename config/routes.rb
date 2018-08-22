83acf1078e8303f1ab6237a5c9550c64c278d23c
tmquang1999 <quang99991111@gmail.com>
2018-08-22 18:55:49 +0700

    repass
    1
    
    1
    
    1
    
    1
    
    1

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: :edit
+  resources :password_resets, except: %i(index show destroy)
   resources :books, except: %i(index new)
   resources :categories
   scope "(:locale)", locale: /en|vi/ do
